70008ccf7dac7ec4a2079e1ad22a4896233166fc
Nedzik <113420019+Nedzik97@users.noreply.github.com>
2023-06-21 14:17:53 +0300

    moved svg icons to separate files

--- src/components/current-weather/Current-weather.jsx
+++ src/components/current-weather/Current-weather.jsx
@@ -1,4 +1,6 @@
 import { getDate, getTime } from "../../utils";
+import windDir from "../../images/icons/wind-direction.svg";
+import windSpeed from "../../images/icons/wind-speed.svg";
 import styles from "./Current-weather.module.scss";
 
 export const CurrentWeather = ({ weatherData, temperatureUnit }) => {
@@ -25,34 +27,21 @@ export const CurrentWeather = ({ weatherData, temperatureUnit }) => {
         </div>
         <div className={styles.windIndicators}>
           <div>
-            <div className={styles.windIcon}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35">
-                <path
-                  fill="#ffffff"
-                  fill-rule="evenodd"
-                  d="M.004 11.797 0 11.788l.004.009zm41.98 6.983-1.258-2.44C23.979 21.885 17.399 3.205.004 11.797l1.25 2.422c17.069-7.589 21.053 11.75 40.73 4.561zm-.001 6.041.017.033-.017-.033zM.004 17.842l1.25 2.42c17.069-7.586 21.053 11.749 40.729 4.559l-1.257-2.434C23.979 27.928 17.399 9.248.004 17.842zm0 6.043 1.25 2.422c17.069-7.588 21.053 11.75 40.73 4.561l-1.258-2.436C23.979 33.973 17.399 15.293.004 23.885z"
-                />
-              </svg>
-            </div>
+            <img
+              className={styles.windIcon}
+              src={windSpeed}
+              alt="wind speed"
+            ></img>
             <p
               className={styles.windSpeed}
             >{`Wind ${weatherData.currentWeather.windKPH} km/h`}</p>
           </div>
           <div>
-            <div className={styles.windIcon}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                space="preserve"
-                viewBox="0 0 35 35"
-                fill="#ffffff"
-              >
-                <circle cx="16" cy="13" r="2" />
-                <path
-                  fill="#ffffff"
-                  d="M17.88 10.661C17.585 7.532 16.792 0 16 0s-1.585 7.532-1.88 10.661C14.635 10.248 15.288 10 16 10s1.365.248 1.88.661zM18.968 12.56a3 3 0 1 1-5.935-.001c-2.556 1.815-8.702 6.279-8.306 6.966.395.687 7.304-2.387 10.166-3.699L14 31s.5 1 2 1 2-1 2-1l-.893-15.173c2.863 1.312 9.771 4.386 10.166 3.7.397-.688-5.75-5.153-8.305-6.967z"
-                />
-              </svg>
-            </div>
+            <img
+              className={styles.windIcon}
+              src={windDir}
+              alt="wind icon"
+            ></img>
             <p
               className={styles.windDir}
             >{`Wind dir ${weatherData.currentWeather.windDir}`}</p>
@@ -70,7 +59,7 @@ export const CurrentWeather = ({ weatherData, temperatureUnit }) => {
           {temperatureUnit === "celsius"
             ? weatherData.currentWeather.tempC
             : weatherData.currentWeather.tempF}
-          {temperatureUnit.celsius ? <sub>ºC</sub> : <sub>ºF</sub>}
+          {temperatureUnit === "celsius" ? <sub>ºC</sub> : <sub>ºF</sub>}
         </p>
         <span className={styles.text}>{weatherData.currentWeather.text}</span>
         <span
--- src/components/location-search-button/location-search-button.jsx
+++ src/components/location-search-button/location-search-button.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { ThemeContext } from "../ThemeContext";
+import choiceLocationIcon from "../../images/icons/choice-location-icon.svg";
 import cx from "classnames";
 import styles from "./location-search-button.module.scss";
 
@@ -13,17 +14,7 @@ export const LocationSearchButton = ({ setIsClickLocation }) => {
       })}
       type="button"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="18"
-        height="17"
-        fill="none"
-      >
-        <path
-          fill="#ffffff"
-          d="M16.785.956.84 7.104C-.248 7.541-.24 8.148.64 8.42l4.094 1.277 9.472-5.976c.448-.272.857-.126.52.173l-7.673 6.925H7.05l.002.001-.283 4.22c.414 0 .597-.19.829-.414l1.988-1.934 4.136 3.055c.762.42 1.31.205 1.5-.706l2.715-12.795c.278-1.114-.425-1.618-1.153-1.29Z"
-        />
-      </svg>
+      <img src={choiceLocationIcon} alt="choice-location icon" />
       Location
     </button>
   );
